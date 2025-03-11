790c4c75c01c439b6c50696e3ae3c7d0fed23b34
AdiyZd <154992866+AdiyZd@users.noreply.github.com>
2025-03-11 13:53:32 +0700

    update foto
    update foto

---
+++ js/.js
@@ -0,0 +1,5 @@
+/* 
+
+    NEXT PENGIRIMAN FOTO KE TELE
+
+*/
--- js/Opencamera.js
+++ js/Opencamera.js
@@ -1,29 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const absenBtn = document.getElementById("absenBtn");
-    const videoStream = document.getElementById("stream");
+  const absenBtn = document.getElementById("absenBtn");
+  const videoStream = document.getElementById("stream");
+  const foto = document.getElementById("ambilFoto");
+  const priviu = document.getElementById("priviu");
+  const context = priviu.getContext("2d");
+  let mati = null;
 
-    absenBtn.addEventListener("click", function () {
-        videoStream.style.display = "block";
+  absenBtn.addEventListener("click", function () {
+    videoStream.style.display = "block";
+    foto.style.display = "block";
 
-        absenBtn.style.display = "none";
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then((streamKamera) => {
-                videoStream.srcObject = streamKamera;
-                videoStream.play();
+    absenBtn.style.display = "none";
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          width: { ideal: 1280 },
+          height: { ideal: 720 },
+          facingMode: "user",
+        },
+      })
+      .then((streamKamera) => {
+        mati = streamKamera;
+        videoStream.srcObject = streamKamera;
+        videoStream.play();
 
-                // Disable tombol setelah kamera aktif
-                absenBtn.disabled = true;
-                absenBtn.innerText = "Kamera Aktif";
-            })
-            .catch((error) => {
-                console.error("Akses Kamera Ditolak: ", error);
-                videoStream.style.display = "none";
-                Swal.fire({
-                    icon: "error",
-                    title: "Akses Kamera Ditolak",
-                    text: "Mohon izinkan akses kamera untuk melakukan absen",
-                    draggable: true
-                });
-            });
-    });
+        // Disable tombol setelah kamera aktif
+        absenBtn.disabled = true;
+        absenBtn.innerText = "Kamera Aktif";
+      })
+      .catch((error) => {
+        console.error("Akses Kamera Ditolak: ", error);
+        videoStream.style.display = "none";
+        Swal.fire({
+          icon: "error",
+          title: "Akses Kamera Ditolak",
+          text: "Mohon izinkan akses kamera untuk melakukan absen",
+          draggable: true,
+        });
+      });
+  });
+
+//   ambil foto
+  foto.addEventListener("click", function() {
+    priviu.width = videoStream.videoWidth;
+    priviu.height = videoStream.videoHeight;
+
+    // frem vidio
+    context.drawImage(videoStream, 0, 0, priviu.width, priviu.height);
+
+    // matikan kamera biyar vidio mati dan tampilan normal lagi
+    if (mati) {
+        let cekCamera = mati.getTracks();
+        cekCamera.forEach((track) => cekCamera.stop()); // stok kamera
+        videoStream.srcObject = null;
+    }
+    
+    // matikan vidio dan tombol ambil foto
+    videoStream.style.display = "none";
+    foto.style.display = "none";
+
+    priviu.style.display = "block";
+  });
 });
+
